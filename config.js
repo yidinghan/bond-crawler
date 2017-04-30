5b8b7015ba9d05d21e21383d740bc1a771064bd5
ding <playdingnow@gmail.com>
2017-04-30 19:38:37 +0800

    chore(package): npm install

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.urls = {
+    see: {
+        bond: 'http://bond.sse.com.cn/bridge/information/',
+        project: 'http://query.sse.com.cn/commonQuery.do?jsonCallBack=jsonpCallback39996&isPagination=true&sqlId=COMMON_SSE_ZCZZQXMLB&pageHelp.pageSize=10&area=&trade=&status=&bond_type=0&begin=&_=1493551986687'
+    },
+};
