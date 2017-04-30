7b12aa0f2d6404ba8090bb923fa8afa6d8dc72f2
ding <playdingnow@gmail.com>
2017-04-30 19:56:24 +0800

    feat: init sites dir

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 exports.urls = {
     see: {
         bond: 'http://bond.sse.com.cn/bridge/information/',
-        project: 'http://query.sse.com.cn/commonQuery.do?jsonCallBack=jsonpCallback39996&isPagination=true&sqlId=COMMON_SSE_ZCZZQXMLB&pageHelp.pageSize=10&area=&trade=&status=&bond_type=0&begin=&_=1493551986687'
+        project: 'http://query.sse.com.cn/commonQuery.do?jsonCallBack=jsonpCallback39996&isPagination=true&sqlId=COMMON_SSE_ZCZZQXMLB&pageHelp.pageSize=10&area=&trade=&status=&bond_type=0&begin=&_=1493551986687',
+    },
+    szse: {
+        project: 'http://www.szse.cn/main/ints/xmjdxx/xgm/',
     },
 };
