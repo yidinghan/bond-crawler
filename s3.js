954ce43ff1c9119f39ec7c9460b1acc3c7074ed5
ding <playdingnow@gmail.com>
2017-05-03 15:56:55 +0800

    feat: use s3 instead of upload

---
+++ s3.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+
+const config = require('./config').s3;
+
+const S3 = new AWS.S3();
+
+const upload = (filename, body) => {
+  const options = {
+    Bucket: config.bucket,
+    Key: filename,
+    Body: body,
+  };
+
+  return S3.upload(options).promise();
+};
+
+module.exports = {
+  S3,
+  upload,
+};
