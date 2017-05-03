7cfc1e63aa040d5bd31eac5361d08d0db4d0d26a
ding <playdingnow@gmail.com>
2017-05-03 15:46:05 +0800

    feat: add lst api

--- handler.js
+++ handler.js
@@ -1,12 +1,25 @@
-const bc = require('./index');
+const AWS = require('aws-sdk');
 
-exports.update = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'successfully!',
-    }),
+const bc = require('./index');
+const config = require('./config');
+
+const S3 = new AWS.S3();
+
+const makeResponse = (callback, error, result) => {
+  const statusCode = (error && error.statusCode) || 200;
+  const body = {
+    statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(result),
   };
 
-  return bc.run().nodeify(err => callback(err, response));
+  callback(error, body);
+};
+
+exports.update = (event, context, callback) => bc.run().nodeify(makeResponse.bind(null, callback));
+
+exports.list = (event, context, callback) => {
+  S3.listObjectsV2({ Bucket: config.s3.bucket }, makeResponse.bind(null, callback));
 };
